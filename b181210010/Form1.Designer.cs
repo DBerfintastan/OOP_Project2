7125da7c68a96620e0b9e3cf1c3186d77ee108ed
Deniz Berfin Taştan <62747201+DBerfintastan@users.noreply.github.com>
2022-04-13 14:27:25 +0300

    Add files via upload

---
+++ b181210010/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace b181210010
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.x_deger = new System.Windows.Forms.Label();
+            this.txtSayiX = new System.Windows.Forms.TextBox();
+            this.y_deger = new System.Windows.Forms.Label();
+            this.txtSayiY = new System.Windows.Forms.TextBox();
+            this.arkadasmi = new System.Windows.Forms.Button();
+            this.son = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // x_deger
+            // 
+            this.x_deger.AutoSize = true;
+            this.x_deger.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.x_deger.Location = new System.Drawing.Point(59, 53);
+            this.x_deger.Name = "x_deger";
+            this.x_deger.Size = new System.Drawing.Size(38, 29);
+            this.x_deger.TabIndex = 1;
+            this.x_deger.Text = "X:";
+            // 
+            // txtSayiX
+            // 
+            this.txtSayiX.Location = new System.Drawing.Point(175, 60);
+            this.txtSayiX.Name = "txtSayiX";
+            this.txtSayiX.Size = new System.Drawing.Size(105, 22);
+            this.txtSayiX.TabIndex = 3;
+            // 
+            // y_deger
+            // 
+            this.y_deger.AutoSize = true;
+            this.y_deger.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.y_deger.Location = new System.Drawing.Point(59, 125);
+            this.y_deger.Name = "y_deger";
+            this.y_deger.Size = new System.Drawing.Size(37, 29);
+            this.y_deger.TabIndex = 4;
+            this.y_deger.Text = "Y:";
+            // 
+            // txtSayiY
+            // 
+            this.txtSayiY.Location = new System.Drawing.Point(175, 125);
+            this.txtSayiY.Name = "txtSayiY";
+            this.txtSayiY.Size = new System.Drawing.Size(105, 22);
+            this.txtSayiY.TabIndex = 5;
+            // 
+            // arkadasmi
+            // 
+            this.arkadasmi.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.arkadasmi.Location = new System.Drawing.Point(26, 191);
+            this.arkadasmi.Name = "arkadasmi";
+            this.arkadasmi.Size = new System.Drawing.Size(117, 41);
+            this.arkadasmi.TabIndex = 6;
+            this.arkadasmi.Text = "ARKADAŞ MI?";
+            this.arkadasmi.UseVisualStyleBackColor = false;
+            this.arkadasmi.Click += new System.EventHandler(this.arkadasmi_Click);
+            // 
+            // son
+            // 
+            this.son.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.son.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.son.Location = new System.Drawing.Point(175, 191);
+            this.son.Name = "son";
+            this.son.Size = new System.Drawing.Size(117, 41);
+            this.son.TabIndex = 7;
+            this.son.Text = "SON";
+            this.son.UseVisualStyleBackColor = false;
+            this.son.Click += new System.EventHandler(this.son_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(316, 289);
+            this.Controls.Add(this.son);
+            this.Controls.Add(this.arkadasmi);
+            this.Controls.Add(this.txtSayiY);
+            this.Controls.Add(this.y_deger);
+            this.Controls.Add(this.txtSayiX);
+            this.Controls.Add(this.x_deger);
+            this.Name = "Form1";
+            this.Text = "ARKADAŞ MI?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label x_deger;
+        private System.Windows.Forms.TextBox txtSayiX;
+        private System.Windows.Forms.Label y_deger;
+        private System.Windows.Forms.TextBox txtSayiY;
+        private System.Windows.Forms.Button arkadasmi;
+        private System.Windows.Forms.Button son;
+    }
+}
+
---
+++ b181210010/Form1.cs
@@ -0,0 +1,106 @@
+﻿/****************************************************************************
+**					SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				    BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				   NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					2019-2020 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........: 2
+**				ÖĞRENCİ ADI............: Deniz Berfin Taştan
+**				ÖĞRENCİ NUMARASI.......: B181210010
+**              DERSİN ALINDIĞI GRUP...: 1-D
+****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace b181210010
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void arkadasmi_Click(object sender, EventArgs e)
+        {
+            this.Width = 822;
+            this.Height = 437; //Ekran yeni boyutunu ayarlar.
+
+            Label lblx = new Label();
+            lblx.Text = "X Bölenleri";
+            lblx.Location = new Point(400, 50);
+            this.Controls.Add(lblx); 
+
+            Label lbly = new Label();
+            lbly.Text = "Y Bölenleri";
+            lbly.Location = new Point(600, 50);
+            this.Controls.Add(lbly); 
+
+            Label toplamlar = new Label();
+            toplamlar.Text = "TOPLAMLAR";
+            toplamlar.Location = new Point(300, 241);
+            this.Controls.Add(toplamlar); //Yeni label oluşturur.
+
+            ListBox x_bolen = new ListBox();
+            x_bolen.Location = new Point(400, 85);
+            this.Controls.Add(x_bolen); // Arkadaş mı butonuna basıldığında yanda x bölenlerinin yazıldığı bir listbox alanı oluşturur.
+
+            ListBox y_bolen = new ListBox();
+            y_bolen.Location = new Point(600, 85);
+            this.Controls.Add(y_bolen); // Arkadaş mı butonuna basıldığında yanda y bölenlerinin yazıldığı bir listbox alanı oluşturur.
+
+
+            int x, y;
+            int toplam1 = 0, toplam2 = 0;
+            x = Convert.ToInt32(txtSayiX.Text);
+            y = Convert.ToInt32(txtSayiY.Text);
+            for (int i = 1; i < x; i++)
+            {
+                if (x % i == 0)
+                {
+
+                    x_bolen.Items.Add(Convert.ToString(i));
+                    toplam1 += i;
+
+                }
+            } // x bölenlerini bulur
+            for (int j = 1; j < y; j++)
+            {
+                if (y % j == 0)
+                {
+
+                    y_bolen.Items.Add(Convert.ToString(j));
+                    toplam2 += j;
+
+                }
+            } // y bölenlerini bulur
+
+            TextBox xtoplam = new TextBox();
+            xtoplam.Location = new Point(400, 240);
+            xtoplam.Text = Convert.ToString(toplam1);
+            xtoplam.Size = new System.Drawing.Size(125, 25);
+            this.Controls.Add(xtoplam); // Yanda x bölenlerinin toplamının yazıldığı bir textbox alanı oluşturur.
+
+            TextBox ytoplam = new TextBox();
+            ytoplam.Location = new Point(600, 240);
+            ytoplam.Text = Convert.ToString(toplam2);
+            xtoplam.Size = new System.Drawing.Size(125, 25);
+            this.Controls.Add(ytoplam); // Yanda y bölenlerinin toplamının yazıldığı bir textbox alanı oluşturur.
+
+        }
+
+        private void son_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
